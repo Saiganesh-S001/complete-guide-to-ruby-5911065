e55b49ee2b7f0bead4a8d198db2a335e9c9fa56d
Saiganesh-S001 <saiganesh.s@ringcentral.com>
2025-02-06 09:17:42 +0000

    Exceptions

--- practise/errors.rb
+++ practise/errors.rb
@@ -6,8 +6,33 @@ end
 
 
 begin
-  puts div(1,0)
-  puts div(4,'s')
+  # puts div(1,0)
+  # puts div(4,'s')
+  puts even_numbers([])
 rescue => e
   puts "#{e.class}.#{e.message}.#{e.backtrace}"
 end
+
+class NoEvensError < StandardError
+  # can extend it
+  attr_reader :arr
+  
+  def initialize(arr=[])
+    @arr = arr
+    super("No even numbers in the array") #og class takes msg as argument
+  end
+end
+
+def even_numbers(arr=[])
+  unless arr.is_a?(Array)
+    raise ArgumentError
+  end
+
+  if arr.length == 0
+    raise StandardError.new("Length of the array cant be zero")
+  end
+  arr.find_all {|e| e.to_i % 2 == 0}
+end
+
+
+
